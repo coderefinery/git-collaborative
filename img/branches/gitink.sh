a6592eefbade626fd09a81c88c3d0ffc1e67d9b8
Radovan Bast <bast@users.noreply.github.com>
2017-05-07 18:50:16 +0200

    rm unused file

--- img/branches/gitink.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -d "gitink" ]; then
-    git clone https://github.com/bast/gitink.git
-fi
-
-for file in *.txt; do
-    python gitink/gitink/gitink.py $file > ${file%txt}svg
-done
